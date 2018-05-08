ef94e94479c5f5a625e8008e0d44048e07feb804
SeSmC06 <dingpeng06@gmail.com>
2018-05-08 16:49:42 -0700

    done

--- ex2/ex2.js
+++ ex2/ex2.js
@@ -1,27 +1,34 @@
-function output(txt) {
-	console.log(txt);
+function not(predicate) {
+  return function negated(...args) {
+    return !predicate(...args);
+  };
 }
 
-function printIf(predicate) {
-	return function(msg) {
-		if (predicate(msg)) {
-			output(msg);
-		}
-	};
+function when(fn) {
+  return function(predicate) {
+    return function(...args) {
+      if (predicate(...args)) {
+        return fn(...args);
+      }
+    };
+  };
 }
+
+// *********************
+
+// only necessary in some browsers
+var output = console.log.bind(console);
+var printIf = when(output);
+var isLongEnough = not(isShortEnough);
 
 function isShortEnough(str) {
-	return str.length <= 5;
+  return str.length <= 5;
 }
 
-function isLongEnough(str) {
-	return !isShortEnough(str);
-}
+var msg1 = 'Hello';
+var msg2 = msg1 + ' World';
 
-var msg1 = "Hello";
-var msg2 = msg1 + " World";
-
-printIf(isShortEnough)(msg1);		// Hello
+printIf(isShortEnough)(msg1); // Hello
 printIf(isShortEnough)(msg2);
 printIf(isLongEnough)(msg1);
-printIf(isLongEnough)(msg2);		// Hello World
+printIf(isLongEnough)(msg2); // Hello World
