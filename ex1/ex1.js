b321606e0dbca51776f0bbf83e2936f4205b5f59
SeSmC06 <dingpeng06@gmail.com>
2018-05-08 16:05:50 -0700

    done

--- ex1/ex1.js
+++ ex1/ex1.js
@@ -1,12 +1,27 @@
 function foo(x) {
-	y++;
-	z = x * y;
+  y++;
+  z = x * y;
 }
 
-var y = 5, z;
+var y = 5,
+  z;
 
 foo(20);
-z;		// 120
+z; // 120
 
 foo(25);
-z;		// 175
+z; // 175
+
+function bar(x, y) {
+  y = y + 1;
+  var z;
+
+  foo(x);
+
+  return [y, z];
+  function foo(x) {
+    z = x * y;
+  }
+
+  // foo returns z
+}
