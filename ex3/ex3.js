dcd22d84e763d0f3b0571936d7d3378705d43f48
SeSmC06 <dingpeng06@gmail.com>
2018-05-08 16:49:46 -0700

    done

--- ex3/ex3.js
+++ ex3/ex3.js
@@ -1,13 +1,37 @@
-function increment(x) { return x + 1; }
-function decrement(x) { return x - 1; }
-function double(x) { return x * 2; }
-function half(x) { return x / 2; }
+function increment(x) {
+  return x + 1;
+}
+function decrement(x) {
+  return x - 1;
+}
+function double(x) {
+  return x * 2;
+}
+function half(x) {
+  return x / 2;
+}
 
-function compose() {}
-function pipe() {}
+function compose(...args) {
+  // right to left
+  return function composed(result) {
+    for (let i = args.length - 1; i >= 0; i--) {
+      result = args[i](result);
+    }
+    return result;
+  };
+}
+function pipe(...args) {
+  // left to right
+  return function piped(result) {
+    for (let i = 0; i < args.length; i++) {
+      result = args[i](result);
+    }
+    return result;
+  };
+}
 
-var f = compose(decrement,double,increment,half);
-var p = pipe(half,increment,double,decrement);
+var f = compose(decrement, double, increment, half);
+var p = pipe(half, increment, double, decrement);
 
 f(3) === 4;
 // true
