78214dd402ff8d541ea157b1ce8155fea3d5305f
SeSmC06 <dingpeng06@gmail.com>
2018-05-08 17:07:34 -0700

    done

--- ex7/ex7.js
+++ ex7/ex7.js
@@ -1,1 +1,23 @@
 // code here! :)
+function valFn(v) {
+  return function() {
+    return v;
+  };
+}
+
+function add(x, y) {
+  return x + y;
+}
+
+function add2(fn1, fn2) {
+  return add(fn1(), fn2());
+}
+
+function addn(fns) {
+  // reduce:
+  return fns.reduce(function reducer(composedFn, fn) {
+    return function() {
+      return add2(composedFn, fn);
+    };
+  })();
+}
